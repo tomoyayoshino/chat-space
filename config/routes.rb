915cd46f27357368978f6d8b0e10e3177d31e733
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-01 14:18:34 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "groups#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update]
 end
