eb33023f6a8f3d107e30357e72e5cf3ff28382c3
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-04 12:30:16 +0900

    edit buildHTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      let html = 
+      `<div class="messageBox">
+        <div class="messageInfo">
+          <div class="messageInfo__userName">
+            ${message.user_name}
+          </div>
+          <div class="messageInfo__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+            <p class="message__content">
+              ${message.content}
+            </p>
+          <img src="${message.image}", class: 'message__image'>
+        </div>
+      </div>`
+      return html;
+    } else {
+      let html = 
+      `<div class="messageBox">
+      <div class="messageInfo">
+        <div class="messageInfo__userName">
+          ${message.user_name}
+        </div>
+        <div class="messageInfo__date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+      </div>
+    </div>`
+      return html;
+    };
+  }
+
   $('.form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,6 +51,10 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      
     })
   })
 });
