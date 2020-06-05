b5438e445261624223b592ab44732fbf2bdfe88c
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-05 11:17:43 +0900

    create autoReloadjs

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -65,36 +65,36 @@ $(function(){
   });
   })
 
-  let reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let last_message_id = $('.messageBox:last').data("message-id");
-    $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
-      if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
-        let insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.mainChat__messageList').append(insertHTML);
-        $('.mainChat__messageList').animate({ scrollTop: $('.mainChat__messageList')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-  };
-  //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
-  setInterval(reloadMessages, 7000);
+  // let reloadMessages = function() {
+  //   //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  //   let last_message_id = $('.messageBox:last').data("message-id");
+  //   $.ajax({
+  //     //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+  //     url: "api/messages",
+  //     //ルーティングで設定した通りhttpメソッドをgetに指定
+  //     type: 'get',
+  //     dataType: 'json',
+  //     //dataオプションでリクエストに値を含める
+  //     data: {id: last_message_id}
+  //   })
+  //   .done(function(messages) {
+  //     // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+  //     if (messages.length !== 0) {
+  //       //追加するHTMLの入れ物を作る
+  //       let insertHTML = '';
+  //       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+  //       $.each(messages, function(i, message) {
+  //         insertHTML += buildHTML(message)
+  //       });
+  //       //メッセージが入ったHTMLに、入れ物ごと追加
+  //       $('.mainChat__messageList').append(insertHTML);
+  //       $('.mainChat__messageList').animate({ scrollTop: $('.mainChat__messageList')[0].scrollHeight});
+  //     }
+  //   })
+  //   .fail(function() {
+  //     alert('error');
+  //   });
+  // };
+  // //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
+  // setInterval(reloadMessages, 7000);
 });
