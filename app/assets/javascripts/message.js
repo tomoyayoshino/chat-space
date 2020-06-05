b3d561e57b70f556f186ffe66f20baec8d98d207
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-05 10:38:15 +0900

    let reloadMessages

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(function(){
+
   function buildHTML(message){
     if (message.image) {
       let html = 
@@ -63,4 +64,24 @@ $(function(){
       alert("メッセージ送信に失敗しました");
   });
   })
+
+  let reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    let last_message_id = $('.messageBox:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
 });
